68c2b8fd0ae4f28d8834b4a698f718971bca37b9
sgangadh <sgangadh@56ebe14e-9609-f449-8404-ff2adf786831>
2016-07-22 12:46:17 +0000

    Animation added in splash screen
    git-svn-id: https://asi-storage07.blr.allegisindia.com/svn/EASi_App-POC@10 56ebe14e-9609-f449-8404-ff2adf786831

--- trunk/EasiCab/SourceCode/EasiCab/app/src/main/java/com/cab/easi/easicab/RegisterActivity.java
+++ trunk/EasiCab/SourceCode/EasiCab/app/src/main/java/com/cab/easi/easicab/RegisterActivity.java
@@ -3,11 +3,14 @@ package com.cab.easi.easicab;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 /**
  * Created by sunayak on 7/22/2016.
  */
-public class RegisterActivity extends ActionBarActivity {
+public class RegisterActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- trunk/EasiCab/SourceCode/EasiCab/app/src/main/java/com/cab/easi/easicab/SplashScreen.java
+++ trunk/EasiCab/SourceCode/EasiCab/app/src/main/java/com/cab/easi/easicab/SplashScreen.java
@@ -1,50 +1,59 @@
 package com.cab.easi.easicab;
 /**
  * Created by sunayak on 7/11/2016.
+ * SplashScreen Activity created.
+ *
+ * @Added: @Sharath on 7/22/2016.
+ * Animation Added in Splashscreen Activity
+ *
  */
+import android.app.Activity;
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.Window;
-import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 
-public class SplashScreen extends ActionBarActivity {
+public class SplashScreen extends Activity {
 
-    private static final int SPLASH_TIME_OUT = 10000/2;
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        Window window = getWindow();
+        window.setFormat(PixelFormat.RGBA_8888);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_spash_screen);
 
-        new Handler().postDelayed(new Runnable() {
-
-			/*
-			 * Showing splash screen with a timer. This will be useful when you
-			 * want to show case your app logo / company
-			 */
-
-            @Override
+        StartAnimations();
+        Thread timerThread = new Thread() {
             public void run() {
-                // This method will be executed once the timer is over
-                // Start your app main activity
-                Intent i = new Intent(SplashScreen.this, LoginActivity.class);
-                startActivity(i);
-
-                // close this activity
-                finish();
+                try {
+                    //Holding....in milleSecond
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
             }
-        }, SPLASH_TIME_OUT);
+        };
+        timerThread.start();
     }
 
+    private void StartAnimations() {
 
-
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.translate);
+        anim.reset();
+        ImageView iv = (ImageView) findViewById(R.id.logo);
+        iv.clearAnimation();
+        iv.startAnimation(anim);
+    }
 }
