e44110c701af6d5263b8db43819e86bb1f5b3b32
sunayak <sunayak@56ebe14e-9609-f449-8404-ff2adf786831>
2016-07-22 12:03:13 +0000

    Upto Select Role Screen
    git-svn-id: https://asi-storage07.blr.allegisindia.com/svn/EASi_App-POC@9 56ebe14e-9609-f449-8404-ff2adf786831

---
+++ trunk/EasiCab/SourceCode/EasiCab/app/src/main/java/com/cab/easi/easicab/RegisterActivity.java
@@ -0,0 +1,18 @@
+package com.cab.easi.easicab;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by sunayak on 7/22/2016.
+ */
+public class RegisterActivity extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selectrole);
+
+    }
+
+}
---
+++ trunk/EasiCab/SourceCode/EasiCab/app/src/main/java/com/cab/easi/easicab/SplashScreen.java
@@ -0,0 +1,50 @@
+package com.cab.easi.easicab;
+/**
+ * Created by sunayak on 7/11/2016.
+ */
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
+
+
+public class SplashScreen extends ActionBarActivity {
+
+    private static final int SPLASH_TIME_OUT = 10000/2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_spash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+
+			/*
+			 * Showing splash screen with a timer. This will be useful when you
+			 * want to show case your app logo / company
+			 */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, LoginActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+
+
+}
